1b29aa3ca6d7cab767f071f376442eb745daa771
Dora Di <tdi@cphbusiness.dk>
2020-09-23 17:43:10 +0200

    si p10

---
+++ code/P10-REST-Level-3/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into student values(101, 'Ronja', 'ro@mail.dk');
+insert into student values(102, 'Bastian', 'ba@mail.dk');
+insert into student values(103, 'Alice', 'al@mail.wld');
+insert into student values(104, 'Bob', 'bo@bobov.dol');
+insert into student values(105, 'Jonathan', 'jo@mail.us');
